2c517d35fc91dd840e07fa01aa3f87e48ec4590f
Sergei Suchkov <rafaelfederer335@gmail.com>
2020-04-13 16:23:42 +0300

    test 12

--- src/index.js
+++ src/index.js
@@ -7,12 +7,20 @@ import '../assets/application.scss';
 import gon from 'gon';
 // import cookies from 'js-cookie';
 // import io from 'socket.io-client';
-import app from './app.jsx';
+// import app from './app.jsx';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
+const app = () => {
+  const mountNode = document.querySelector('body');
+  const el = document.createElement('div');
+  el.textContent = 'works';
+  mountNode.append(el)
+};
+
 console.log('it works!');
 console.log('gon', gon);
-app(gon);
+// app(gon);
+app();
